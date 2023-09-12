b6d08c40ead158414bff175215618780ba1b8959
Ho Duc Vu <hoducvu1234@gmail.com>
2023-09-12 17:21:09 +0700

    container

--- lib/main.dart
+++ lib/main.dart
@@ -20,52 +20,29 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.deepOrange.shade700,
       ),
 
-      body: //const Text('OK, I\'m right'),
-        Center( // use this to center body app
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Icon(Icons.signal_wifi_statusbar_connected_no_internet_4_sharp,
-            color: Colors.greenAccent,
-            size: 100.0),
-            ElevatedButton(
-              onPressed: () {
-
-              }, 
-              child: const Column( 
-                children: [
-                Text('Click me now!!!'),
-                Icon(Icons.bubble_chart),
-                ]
-              ),
-              
-            ),
-            const SizedBox(height: 30),
-
-            ElevatedButton.icon(
-              onPressed: () {}, 
-              icon: const Icon(
-                Icons.access_alarm_outlined
-                ), 
-              label: const Text('Alarm'),
-              style:ElevatedButton.styleFrom(
-                backgroundColor: Colors.blueAccent),
-            ),
-
-            const SizedBox(height: 30),
-
-            IconButton(
-              onPressed: () {}, 
-              icon: const Icon(Icons.mail),
-              color: Colors.greenAccent),
-          ],
+      body: 
+      
+        Container(
+          padding: const EdgeInsets.fromLTRB(20.0, 30.0, 20.0, 30.0),// can chinh childs. all(),...
+          margin: const EdgeInsets.all(30.0), //can chinh vi tri Container
+          color: Colors.white,
+          child: const Text('Xin chao!!', selectionColor: Colors.amber,),
         )
-      ),
-      floatingActionButton:  FloatingActionButton(onPressed: () {
-       
-      },
-      child: const Text('Click'),),
-      backgroundColor: Colors.deepOrange[200],
+      ,
+      /*
+      const Padding(
+        padding: EdgeInsets.all(50.0),
+        child: Text('XIN CHAO'),
+        )
+      ,
+      */
+      floatingActionButton:  FloatingActionButton(
+        onPressed: () {
+        
+        },
+        child: const Text('Click'),
+        ),
+        backgroundColor: Colors.deepOrange[200],
     );
   }
 }
